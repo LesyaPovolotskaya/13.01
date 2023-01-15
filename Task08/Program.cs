2c374dbd956fd85567f36ce8c7649a40e4055a73
Lesya <povolotslaya.ll@mail.ru>
2023-01-15 13:44:32 +0300

    Correction Tast08

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -4,11 +4,26 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
-Console.WriteLine("Введите число больше 1: ");
+// Console.WriteLine("Введите число больше 1: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int count = 2;
+// while (count <= number)
+// {
+//     Console.Write(count + " ");
+//     count = count + 2;
+// }
+
+
+Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int count = 2;
+
 while (count <= number)
 {
     Console.Write(count + " ");
     count = count + 2;
+}
+if (number <= 1)
+{
+    Console.WriteLine("ERROR! Число должно быть больше 1");
 }
