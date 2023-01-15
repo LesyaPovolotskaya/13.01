07fbfb565d72bb1b29020c5d879eada05359624e
Lesya <povolotslaya.ll@mail.ru>
2023-01-15 13:31:19 +0300

    Correction Tesk08

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -4,7 +4,7 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
-Console.WriteLine("Введите число: ");
+Console.WriteLine("Введите число больше 1: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int count = 2;
 while (count <= number)
