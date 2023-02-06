31ab7f7051aa0a2d99c05e06494d7be77d6def62
Lesya <povolotslaya.ll@mail.ru>
2023-02-06 15:18:19 +0300

    add new corrections

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -12,46 +12,53 @@ double[] CreateArrayRndDouble(int size, int min, int max)
     for (int i = 0; i < arr.Length; i++)
     {
         double num = rnd.NextDouble() * (max - min) + min;
-        arr[i] = Math.Round(num, 1);
+        arr[i] = Math.Round(num, 2);
     }
     return arr;
 }
 
-void PrintArrayDouble(int[] arr)
+void PrintArrayDouble(double[] arr)
 {
     Console.Write("[");
     for (int i = 0; i < arr.Length; i++)
     {
-        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
-        else Console.Write($"{arr[i]}");
+        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i]);
     }
     Console.Write("]");
 }
 
 
-double MinMaxNumbersArrayDouble(int[] arr)
+double MaxElementArray(double[] arr)
 {
-    double minNumber = arr[0];
-    double maxNumber = arr[0];
+    double maxElement = arr[0];
     for (int i = 0; i < arr.Length; i++)
     {
-        if (arr[i] > maxNumber)
-        {
-            maxNumber = arr[i];
-        }
-        if (arr[i] < minNumber)
-        {
-            minNumber = arr[i];
-        }
-        
-        
+        if (arr[i] > maxElement) maxElement = arr[i];
     }
-     double result = maxNumber - minNumber;
-    return result;
+    Console.WriteLine();
+    Console.WriteLine($"MAX => {maxElement}");
+    return maxElement;
 }
 
-double[] arr = CreateArrayRndDouble(5, 5, 10);
-PrintArrayDouble(arr);
-double result = MinMaxNumbersArrayDouble(arr);
-Console.WriteLine(result);
 
+double MinElementArray(double[] arr)
+{
+    double minElement = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < minElement) minElement = arr[i];
+    }
+    // Console.WriteLine();
+    Console.Write($"MIN => {minElement}");
+    return minElement;
+}
+
+
+
+double [] array = CreateArrayRndDouble(7, 2, 11);
+PrintArrayDouble(array);
+// double DifferenceMaxMinElem = MaxElementArray(array) - MinElementArray(array);
+double DifferenceMaxMinElem = Math.Round(MaxElementArray(array) - MinElementArray(array), 2);
+Console.WriteLine();
+Console.WriteLine($"Разница между MAX и MIN элементами массива -> {DifferenceMaxMinElem}");
